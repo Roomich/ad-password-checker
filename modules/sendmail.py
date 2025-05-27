4673efeb8d3f23c1b1148695d0e730371f502760
Roman Odinichenko <i@odirs.ru>
2025-05-27 15:53:01 +0300

    modifymail func

--- modules/sendmail.py
+++ modules/sendmail.py
@@ -1,29 +1,59 @@
 import smtplib
 import os
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 
 from dotenv import load_dotenv
 
-load_dotenv()
 
-mail_server = os.getenv('MAIL_SERVER', '')
-login = os.getenv('MAIL_LOGIN', '')
-password = os.getenv('MAIL_PASS', '')
-server_port = 587
+def send_email(text, mail_server, server_port,
+    login, password, filename, to, subject,
+):
+    message = MIMEMultipart()
+    message['From'] = login
+    message['To'] = to
+    message['Subject'] = subject
+    message.attach(MIMEText(text, 'plain'))
 
-tolist=['your@mail.any']
+    with open(filename, 'rb') as file:
+        part = MIMEBase('application', 'octet-stream')
+        part.set_payload(file.read())
+        encoders.encode_base64(part)
+        part.add_header(
+            'Content-Disposition',
+            f"attachment; filename={filename}"
+        )
 
-msg='''\
-From: PasswordChecker
-Subject: Duck
+    message.attach(part)
 
-hello my friend!
-this is a test message!
+    with smtplib.SMTP(mail_server, server_port) as smtpObj:
+        smtpObj.starttls()
+        smtpObj.login(login, password)
+        try:
+            smtpObj.send_message(message)
+        finally:
+            smtpObj.quit()
+
+
+if __name__ == '__main__':
+    
+    load_dotenv()
+    
+    filename = 'filename.xlsx'
+    mail_server = os.getenv('MAIL_SERVER', '')
+    login = os.getenv('MAIL_LOGIN', '')
+    password = os.getenv('MAIL_PASS', '')
+    server_port = os.getenv('MAIL_SERVER_PORT', '')
+    to = 'roman.odinichenko@nces.by'
+    subject = 'Учётные записи с истекающим сроком действия пароля'
+    text = '''
+Hello my friend!
+This is a test message!
 Do not respond it!
 '''
-with smtplib.SMTP(mail_server, server_port) as smtpObj:
-    smtpObj.starttls()
-    smtpObj.login(login, password)
-    try:
-        smtpObj.sendmail(login, tolist, msg)
-    finally:
-        smtpObj.quit()
+
+    send_email(text, mail_server, server_port,
+        login, password, filename, to, subject,
+    )
