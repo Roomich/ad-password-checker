749bd3a5f381481c947809ff3549a24e3b0841af
Roman Odinichenko <i@odirs.ru>
2025-05-27 16:17:49 +0300

    add send_mail to main

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from ldap3 import Server, Connection
 from dotenv import load_dotenv
 
+from modules.sendmail import send_email
 from modules.functions import (
     get_pass_lifetime_left,
     sort_users,
@@ -48,8 +49,22 @@ def get_users_list():
 def main():
     data = get_users_list()
     if data:
-        export_to_excel(data)
-    # os.remove('filename.xlsx')
+        filename = export_to_excel(data)
+        mail_server = os.getenv('MAIL_SERVER', '')
+        login = os.getenv('MAIL_LOGIN', '')
+        password = os.getenv('MAIL_PASS', '')
+        server_port = os.getenv('MAIL_SERVER_PORT', '')
+        to = 'roman.odinichenko@nces.by'
+        subject = 'Учётные записи с истекающим сроком действия пароля'
+        text = '''
+Hello my friend!
+This is a test message!
+Do not respond it!
+'''
+        send_email(text, mail_server, server_port,
+            login, password, filename, to, subject,
+        )
+    os.remove('filename.xlsx')
 
 
 if __name__ == '__main__':
